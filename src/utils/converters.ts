a138635287907dccb45b67fa90a4758176bced92
Timur Badretdinov <destinerx@gmail.com>
2022-09-22 17:13:11 +0400

    Change BLOCK_TIME to 12 (post merge)

--- src/utils/converters.ts
+++ src/utils/converters.ts
@@ -1,5 +1,5 @@
 const HOUR = 60 * 60;
-const BLOCK_TIME = 14;
+const BLOCK_TIME = 12;
 
 function blocksToHours(blocks: number): number {
   return parseFloat(((blocks * BLOCK_TIME) / HOUR).toFixed(2));
